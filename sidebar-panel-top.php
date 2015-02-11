40dcee7b7ff224a256a563c9c615c1f1f9b7e003
jeturian <jeturian@gmail.com>
2015-02-11 11:09:14 +0800

    fix top panel class

--- sidebar-panel-top.php
+++ sidebar-panel-top.php
@@ -7,25 +7,25 @@
 ?>
 <div id="panel-top" class="row" role="complementary">
 	<?php if(is_active_sidebar('panel-top-1')): ?>
-	<aside id="sidebar" class="<?php govph_displayoptions( 'govph_position_panel_top_1' ); ?>columns" role="complementary">
+	<aside id="panel-top-1" class="<?php govph_displayoptions( 'govph_position_panel_top_1' ); ?>columns" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php dynamic_sidebar( 'panel-top-1' ) ?>
 	</aside>
 	<?php endif; ?>
 	<?php if(is_active_sidebar('panel-top-2')): ?>
-	<aside id="sidebar" class="<?php govph_displayoptions( 'govph_position_panel_top_2' ); ?>columns" role="complementary">
+	<aside id="panel-top-2" class="<?php govph_displayoptions( 'govph_position_panel_top_2' ); ?>columns" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php dynamic_sidebar( 'panel-top-2' ) ?>
 	</aside>
 	<?php endif; ?>
 	<?php if(is_active_sidebar('panel-top-3')): ?>
-	<aside id="sidebar" class="<?php govph_displayoptions( 'govph_position_panel_top_3' ); ?>columns" role="complementary">
+	<aside id="panel-top-3" class="<?php govph_displayoptions( 'govph_position_panel_top_3' ); ?>columns" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php dynamic_sidebar( 'panel-top-3' ) ?>
 	</aside>
 	<?php endif; ?>
 	<?php if(is_active_sidebar('panel-top-4')): ?>
-	<aside id="sidebar" class="<?php govph_displayoptions( 'govph_position_panel_top_4' ); ?>columns" role="complementary">
+	<aside id="panel-top-4" class="<?php govph_displayoptions( 'govph_position_panel_top_4' ); ?>columns" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php dynamic_sidebar( 'panel-top-4' ) ?>
 	</aside>
