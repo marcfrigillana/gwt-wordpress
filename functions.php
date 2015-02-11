77858e48698edd4d45d7926488eccb0f7a7499cc
jeturian <jeturian@gmail.com>
2015-02-11 10:29:47 +0800

    /var/www/test_wordpress/wp-content/themes/gwt-wordpress-6.0

--- functions.php
+++ functions.php
@@ -277,32 +277,32 @@ function gwt_wp_widgets_init() {
     'id'            => 'footer-1',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
   ) );
   register_sidebar( array(
     'name'          => __( 'Agency Footer 2', 'gwt_wp' ),
     'id'            => 'footer-2',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
   ) );
   register_sidebar( array(
     'name'          => __( 'Agency Footer 3', 'gwt_wp' ),
     'id'            => 'footer-3',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
   ) );
   register_sidebar( array(
     'name'          => __( 'Agency Footer 4', 'gwt_wp' ),
     'id'            => 'footer-4',
     'before_widget' => '<div id="%1$s" class="widget %2$s">',
     'after_widget'  => '</div>',
-    'before_title'  => '<h2 class="widget-title">',
-    'after_title'   => '</h2>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
   ) );
 }
 add_action( 'widgets_init', 'gwt_wp_widgets_init' );
