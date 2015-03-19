c4150ff007e195188e22619d16d7b78993976282
jeturian <jeturian@gmail.com>
2015-03-19 11:12:58 +0800

    modified migrate gwtfooter CDN

--- footer.php
+++ footer.php
@@ -100,14 +100,13 @@ else{
 <!-- standard footer -->
 <div id="gwt-standard-footer"></div>
 <script type="text/javascript">
-    (function(d, s, id) {
-      var js, gjs = d.getElementById('gwt-standard-footer');
+(function(d, s, id) {
+  var js, gjs = d.getElementById('gwt-standard-footer');
 
-      js = d.createElement(s); js.id = id;
-      js.src = "http://gwt-footer.googlecode.com/git/footer.js";
-      gjs.parentNode.insertBefore(js, gjs);
-
-    }(document, 'script', 'gwt-footer-jsdk'));
+  js = d.createElement(s); js.id = id;
+  js.src = "//cdn.i.gov.ph/gwt-footer/footer.js";
+  gjs.parentNode.insertBefore(js, gjs);
+}(document, 'script', 'gwt-footer-jsdk'));
 </script>
 <!-- end standard footer -->
 <?php /*
